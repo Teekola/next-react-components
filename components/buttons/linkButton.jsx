5fb130edb82e62d334bb65b3ba007b001f1ecc5d
Teemu Kerkola <tokerk@utu.fi>
2022-03-20 19:24:24 +0200

    added link button

---
+++ components/buttons/linkButton.jsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledA = styled.a`
+    --background: ${({ theme }) => theme.colors.cta};
+    --backgroundHover: ${({ theme }) => theme.colors.ctaHover};
+    --text: ${({ theme }) => theme.colors.ctaText};
+    --textHover: ${({ theme }) => theme.colors.ctaTextHover};
+    --shadow: ${({ theme }) => theme.shadows.default};
+
+    text-decoration: none;
+    background: var(--background);
+    border-radius: 1rem;
+    padding: .5rem 1rem;
+    display: flex;
+    justify-content: center;
+    color: var(--text);
+    font-weight: bold;
+
+    :hover {
+        cursor: pointer;
+        color: var(--textHover);
+        background: var(--backgroundHover);
+    }
+
+    :active {
+        transform: scale(.95);
+    }
+
+    :focus:not(:focus-visible) {
+        outline: none;
+    }
+`
+
+export default function LinkButton({ href, visibleUrl, children }) {
+    return (
+        <Link href={href} as={visibleUrl} passHref>
+            <StyledA draggable="false">{children}</StyledA>
+        </Link>
+    )
+}
